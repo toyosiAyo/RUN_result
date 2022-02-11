f73effe6b3f2f0991263c04dc8e3340548b8d021
toyxy <toyxyboy@gmail.com>
2022-02-11 17:23:10 +0100

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.auth-layout") 
 
 @section("title")
-:: RUN Result - Login ::
+:: RUN Result - Create Account ::
 @endsection
 
 @section("content")
@@ -9,7 +9,7 @@
     <div id="mytask-layout" class="theme-indigo">
 
         <!-- main body area -->
-        <div class="main p-2 py-3 p-xl-5 ">
+        <div class="main p-2 py-3 p-xl-5">
             
             <!-- Body: Body -->
             <div class="body d-flex p-0 p-xl-5">
@@ -34,15 +34,14 @@
                                 </div>
                             </div>
                         </div>
+
                         <div class="col-lg-6 d-flex justify-content-center align-items-center border-0 rounded-lg auth-h100">
                             <div class="w-100 p-3 p-md-5 card border-0 bg-dark text-light" style="max-width: 32rem;">
                                 <!-- Form -->
                                 <form class="row g-1 p-3 p-md-4">
                                     <div class="col-12 text-center mb-1 mb-lg-5">
-                                        <h1>Sign in</h1>
-                                        <span>Enter your credentials to continue.</span>
+                                        <h1>Create your account</h1>
                                     </div>
-                                    
                                     <div class="col-12">
                                         <div class="mb-2">
                                             <label class="form-label">Email address</label>
@@ -51,28 +50,21 @@
                                     </div>
                                     <div class="col-12">
                                         <div class="mb-2">
-                                            <div class="form-label">
-                                                <span class="d-flex justify-content-between align-items-center">
-                                                    Password
-                                                    <a class="text-secondary" href="forgot">Forgot Password?</a>
-                                                </span>
-                                            </div>
-                                            <input type="password" class="form-control form-control-lg" placeholder="***************">
+                                            <label class="form-label">Password</label>
+                                            <input type="email" class="form-control form-control-lg" placeholder="8+ characters required">
                                         </div>
                                     </div>
                                     <div class="col-12">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                            <label class="form-check-label" for="flexCheckDefault">
-                                                Remember me
-                                            </label>
+                                        <div class="mb-2">
+                                            <label class="form-label">Confirm password</label>
+                                            <input type="email" class="form-control form-control-lg" placeholder="8+ characters required">
                                         </div>
                                     </div>
                                     <div class="col-12 text-center mt-4">
-                                        <a href="index.html" class="btn btn-lg btn-block btn-light lift text-uppercase" atl="signin">SIGN IN</a>
+                                        <button type="submit" class="btn btn-lg btn-block btn-light lift text-uppercase" alt="SIGNUP">CREATE ACCOUNT</button>
                                     </div>
                                     <div class="col-12 text-center mt-4">
-                                        <span class="text-muted">Don't have an account yet? <a href="register" class="text-secondary">Create account here</a></span>
+                                        <span class="text-muted">Already have an account? <a href="/" title="Log in" class="text-secondary">Log in here</a></span>
                                     </div>
                                 </form>
                                 <!-- End Form -->
@@ -84,4 +76,3 @@
         </div>
     </div>
 @endsection
-
