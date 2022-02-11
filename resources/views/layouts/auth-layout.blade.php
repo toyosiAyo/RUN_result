b55dcabc79965ebbe6789cbf2b9a3a5666a17cf4
toyxy <toyxyboy@gmail.com>
2022-02-11 12:49:59 +0100

    update

---
+++ resources/views/layouts/auth-layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <title>@yield('title')</title>
+        <link rel="icon" href="../run_logo.png" type="image/x-icon"> <!-- Favicon-->
+        <!-- project css file  -->
+        <link rel="stylesheet" href="../assets/css/my-task.style.min.css">
+    </head>
+    <body>
+        @yield('content')
+        <script src="../assets/bundles/libscripts.bundle.js"></script>
+    </body>
+</html>
